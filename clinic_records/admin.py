9a4c3d0329cf78d3045134e8011bfc85861d3dd0
fatemearyaie <fatemearyaie9@gmail.com>
2025-08-29 22:57:23 +0330

    register model in admin.py

--- clinic_records/admin.py
+++ clinic_records/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from .models import Patient, Pictures
+from .models import Patient, Pictures, ScalingProphy
 # Register your models here.
 class PatientAdmin(admin.ModelAdmin):
     list_display = ['case_id', 'patient_FullName', 'visit_date', 'disease']
 
 class PicturesAdmin(admin.ModelAdmin):
     list_display = ['opg', 'photo']
+class ScalingProphyAdmin(admin.ModelAdmin):
+    list_display = ['scalingprophy_price', 'sessions_count']
+
+admin.site.register(ScalingProphy, ScalingProphyAdmin)
 admin.site.register(Pictures, PicturesAdmin)
 admin.site.register(Patient, PatientAdmin)
