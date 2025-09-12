a20ea7ccf08fb2de54093c0ae955c4960f92fd69
fatemearyaie <fatemearyaie9@gmail.com>
2025-09-12 17:46:22 +0330

    create forms.py

--- clinic_records/forms.py
+++ clinic_records/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 from django.utils import timezone
+from .models import Patient, ScalingProphy, Pictures
 
 class PatientForm(forms.ModelForm):
-    case_id = forms.CharField(max_length=255)
-    patient_FullName = forms.CharField(max_length=25)
-    visit_date = forms.DateField(default=timezone.now)
-    disease = forms.CharField(max_length=500, null=True, blank=True)
+    class Meta:
+        model = Patient  
+        fields = ['case_id','patient_FullName','visit_date','visit_date']
