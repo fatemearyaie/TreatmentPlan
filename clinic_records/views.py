b3242d251a256156587c45a87b7ca1c38db76dcb
fatemearyaie <fatemearyaie9@gmail.com>
2025-09-12 13:02:14 +0330

    migrations

--- clinic_records/views.py
+++ clinic_records/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Patient, ScalingProphy, Pictures
 # Create your views here.
+def PatientsBasicRecord(request):
+    pass
