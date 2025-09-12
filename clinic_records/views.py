04cc59a3ce74c1458f68351a3959c020bc68d721
fatemearyaie <fatemearyaie9@gmail.com>
2025-09-12 17:45:56 +0330

    add a new view to create

--- clinic_records/views.py
+++ clinic_records/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Patient, ScalingProphy, Pictures
+from .forms import PatientForm
+from django.db import transaction
+
 # Create your views here.
 def PatientsBasicRecord(request):
-    pass
+    return render(request, 'treatmentplan.html')
