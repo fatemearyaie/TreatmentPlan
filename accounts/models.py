65f0deb54fd31cd7ee5376bbd1b2e08acd1be2ba
fatemearyaie <fatemearyaie9@gmail.com>
2025-09-12 12:58:36 +0330

    add user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class UserControl(models.Model):
+    name = models.CharField(max_length=50)
+    family = models.CharField(max_length=50)
+    branch = models.CharField(max_length=50)
+    created_dt = models.DateTimeField(auto_now_add=True)
+    modified_df = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.family} - {self.name}'
