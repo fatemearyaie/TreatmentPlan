68c9322ab4ccfa8679968d043f5212dc23089be4
fatemearyaie <fatemearyaie9@gmail.com>
2025-09-13 09:46:51 +0330

    update models

--- clinic_records/models.py
+++ clinic_records/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 # Create your models here.
 class Patient(models.Model):
-    case_id = models.CharField(max_length=255)
+    case_id = models.CharField(max_length=255, primary_key=True)
     patient_FullName = models.CharField(max_length=30)
     visit_date = models.DateField(default=timezone.now)
     disease = models.CharField(max_length=500, null=True, blank=True)
