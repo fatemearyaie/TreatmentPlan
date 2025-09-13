d39841a031cac3652379f9c77bbcfb48a0857b0c
fatemearyaie <fatemearyaie9@gmail.com>
2025-09-13 09:46:07 +0330

    update PatientForm PictureForm ScalingProphyForm

--- clinic_records/forms.py
+++ clinic_records/forms.py
@@ -5,4 +5,14 @@ from .models import Patient, ScalingProphy, Pictures
 class PatientForm(forms.ModelForm):
     class Meta:
         model = Patient  
-        fields = ['case_id','patient_FullName','visit_date','visit_date']
+        fields = ['case_id','patient_FullName','visit_date','disease']
+
+class PictureForm(forms.ModelForm):
+    class Meta:
+        model = Pictures
+        fields = ['bite_wing1','bite_wing2','bite_wing3','bite_wing4', 'PA1','PA2','PA3','PA4','opg','photo','pictures_price','picture_note','patient']
+
+class ScalingProphyForm(forms.ModelForm):
+    class Meta:
+        model = ScalingProphy
+        fields = ['sessions_count', 'scalingprophy_price', 'scalingprophy_note']
