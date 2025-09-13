b860e1341d0b53fc04d9896b65d1334f2d6bfd42
fatemearyaie <fatemearyaie9@gmail.com>
2025-09-13 09:42:36 +0330

    update views

--- clinic_records/views.py
+++ clinic_records/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Patient, ScalingProphy, Pictures
-from .forms import PatientForm
+from .forms import PatientForm, PictureForm, ScalingProphyForm
 from django.db import transaction
 
 # Create your views here.
 def PatientsBasicRecord(request):
-    return render(request, 'treatmentplan.html')
+    if request.method == "POST":
+        patient_form = PatientForm(request.POST)
+        pictures_form = PictureForm(request.POST)
+        scalingprophy_form = ScalingProphyForm(request.POST)
+        if patient_form.is_valid():
+            patient_form.save()
+        if pictures_form.is_valid():
+            pictures_form.save()
+        if scalingprophy_form.is_valid():
+            scalingprophy_form.save()
+        return redirect('success')
+    else:
+        patient_form = PatientForm()
+        pictures_form = PictureForm()
+        scalingprophy_form = ScalingProphyForm()
+    context = {
+        'patient_form' : patient_form, 
+        'pictures_form' : pictures_form,
+        'scalingprophy_form':scalingprophy_form,
+    }
+    
+    return render(request, 'treatmentplan.html', context)
+
+def SuccessView(request):
+    return HttpResponse('success!')
